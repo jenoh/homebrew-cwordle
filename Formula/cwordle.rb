da6893209275664c6b8909f22b8e229a4ee4019e
jenoh <memint.contact@gmail.com>
2022-03-06 18:48:30 +0100

    maj again

--- Formula/cwordle.rb
+++ Formula/cwordle.rb
@@ -1,8 +1,8 @@
-class Jenohwordle < Formula
+class Cwordle < Formula
     desc "A wordle game write in c++"
-    homepage "https://github.com/jenoh/jenohwordle"
+    homepage "https://github.com/jenoh/cwordle"
     url "https://github.com/jenoh/jenohwordle/releases/download/v1.1.0/wordle.tar.gz"
-    sha256 "560ea26a591cd3af4eeeb742810dccbddf4ab370b5cf4ea4808b2cef41f677f"
+    sha256 "560ea26a591cd3af4eeeb742810dccbddf4ab370b5cf4ea4808b2cef41f677f3"
     version "0.1.0"
   
     def install
